fe4c81d9faa60d00c5eb2fac4014dbba664f00d1
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2023-03-15 16:50:19 -0300

    Fix: Lint

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,2 +1,3 @@
-db.resumoProdutos.insertOne({ franquia: "McDonalds", totalProdutos: { countDocuments: { from: "produtos" } } });
+db.resumoProdutos.insertOne({ franquia: "McDonalds",
+  totalProdutos: { countDocuments: { from: "produtos" } } });
 db.resumoProdutos.find({}, { franquia: 1, totalProdutos: 1, _id: 0 });
