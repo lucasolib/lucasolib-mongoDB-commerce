c4e0bccc4c345f234761ed1e915d850d70d0232e
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2023-03-15 16:46:24 -0300

    Requisito 19: remover todos os ingredientes com nome de cebola

---
+++ challenges/desafio16.js
@@ -0,0 +1,4 @@
+db.produtos.updateOne({ name: "Big Mac" },
+  { $currentDate: { ultimaModificacao: true } });
+db.produtos.find({ ultimaModificacao: { $exists: true } },
+  { nome: 1, _id: 0 });
---
+++ challenges/desafio17.js
@@ -0,0 +1,2 @@
+db.resumoProdutos.insertOne({ franquia: "McDonalds", totalProdutos: { countDocuments: { from: "produtos" } } });
+db.resumoProdutos.find({}, { franquia: 1, totalProdutos: 1, _id: 0 });
